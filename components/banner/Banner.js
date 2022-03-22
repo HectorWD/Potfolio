624d7e82b8708f344338879611e1f9b9727a9a79
Hector Wong <hectorwong2110@gmail.com>
2022-03-21 17:19:19 -0700

    Arreglando lick de cv

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,54 +1,56 @@
 import { motion } from "framer-motion"
+import Link from 'next/link'
 
 
 const Banner = () => {
-    const variantBanner={
-        hidden:{opacity:0,y:-10},
-        visible:{opacity:1,y:0},
-      }
-      const variantBannerItems={
-        hidden:{opacity:0,x:-10},
-        visible:{opacity:1,x:0},
-      }
+  const variantBanner = {
+    hidden: { opacity: 0, y: -10 },
+    visible: { opacity: 1, y: 0 },
+  }
+  const variantBannerItems = {
+    hidden: { opacity: 0, x: -10 },
+    visible: { opacity: 1, x: 0 },
+  }
   return (
     <>
-    <motion.div
-    variants={variantBanner}
-    initial='hidden'
-    animate='visible'
-    transition={{delay:.2}}
-    className='flex flex-col items-center justify-center h-screen px-12 md:flex-row bg-slate-700 '
-    >
-      <motion.img
-      src="/img/Perfilado.jpg"
-      className="w-64 h-64 rounded-full md:mr-4"
-      />
-      <motion.div className="flex flex-col items-center justify-center">
-      <motion.h2
-      variants={variantBannerItems}
-      transition={{delay:.4}}
-      className='text-2xl text-justify text-sky-200'
-      >Hi! I&apos;m Hector Wong WebDeveloper
-      </motion.h2>
-      <motion.a
-      variants={variantBannerItems}
-      whileHover={{scale:1.1}}
-      className="px-4 py-2 my-2 font-semibold bg-green-400 rounded md:ml-10 text-slate-700"
-      target={'_blank'}
-      href={'/cv/CV-Hector Wong.pdf'}
-      >Download CV
-      </motion.a>
+      <motion.div
+        variants={variantBanner}
+        initial='hidden'
+        animate='visible'
+        transition={{ delay: .2 }}
+        className='flex flex-col items-center justify-center h-screen px-12 md:flex-row bg-slate-700 '
+      >
+        <motion.img
+          src="/img/Perfilado.jpg"
+          className="w-64 h-64 rounded-full md:mr-4"
+        />
+        <motion.div className="flex flex-col items-center justify-center">
+          <motion.h2
+            variants={variantBannerItems}
+            transition={{ delay: .4 }}
+            className='text-2xl text-justify text-sky-200'
+          >Hi! I&apos;m Hector Wong WebDeveloper
+          </motion.h2>
+          <motion.a
+            variants={variantBannerItems}
+            whileHover={{ scale: 1.1 }}
+            className="px-4 py-2 my-2 font-semibold bg-green-400 rounded md:ml-10 text-slate-700"
+            target={'_blank'}
+            href={'/cv/CV-HectorWong.pdf'}
+          >
+            Download CV
+          </motion.a>
+        </motion.div>
+
       </motion.div>
-       
-    </motion.div>
-   <motion.div
-   initial={{opacity:0, x:'-100vh'}}
-   animate={{opacity:1, x:0}}
-   transition={{delay:.5, duration:.5,}}
-   >
-     
-     
-   </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: '-100vh' }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: .5, duration: .5, }}
+      >
+
+
+      </motion.div>
     </>
   )
 }
